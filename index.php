61eab8ff4f55213203dd31ea979f852fc00405a2
root <ngomfa@gmail.com>
2018-11-19 15:04:18 +0100

    exo_6

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="fr" dir="ltr">
+<head>
+  <meta charset="utf-8">
+  <title>PHP Partie2_Exo6</title>
+</head>
+<body>
+    <?php
+//Traduire ce code avec des if et des else
+/*<?php
+  echo ($age >= 18) ? 'Tu es majeur' : 'Tu n\'es pas majeur';
+?>
+*/
+
+        $age = 18;
+      if($age >=18){
+        echo "Tu es majeur";
+      }else{
+          echo "Tu n'es pas majeur";
+      }
+    ?>
+</body>
+</html>
